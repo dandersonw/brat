1bf0b4ece88e488873dfb63e5cdce017f8ff168e
Derick Anderson <dericka@allenai.org>
2017-05-22 16:07:02 -0700

    Take multiple files to measure_agreement

--- tools/find_discontinuous_entities.py
+++ tools/find_discontinuous_entities.py
@@ -5,6 +5,12 @@ import argparse
 import diff_and_mark
 from sys import path as sys_path
 import os.path
+import itertools
+import codecs
+import sys
+
+UTF8Writer = codecs.getwriter('utf8')
+sys.stdout = UTF8Writer(sys.stdout)
 
 
 try:
@@ -22,9 +28,29 @@ def find_discontinuous(text_annotation):
     return [e for e in text_annotation.get_entities() if len(e.spans) > 1]
 
 
+def print_entity_mention(entity, source_text):
+    sys.stdout.write("\n" + "-" * 10 + "\n")
+    WINDOW = 20
+    GREEN = "\033[0;32m"
+    END_COLOR = "\033[0;0m"
+    leftmost = max(0, min(itertools.chain.from_iterable(entity.spans)) - WINDOW)
+    rightmost = min(len(source_text), max(itertools.chain.from_iterable(entity.spans)) + WINDOW)
+    i = leftmost
+    for span in entity.spans:
+        sys.stdout.write(source_text[i: span[0]])
+        sys.stdout.write(GREEN)
+        sys.stdout.write(source_text[span[0]: span[1]])
+        sys.stdout.write(END_COLOR)
+        i = span[1]
+
+    sys.stdout.write(source_text[i:rightmost])
+    sys.stdout.flush()
+
+
 def main(argv=None):
     ap = argparse.ArgumentParser(description="Find entities with discontinuous spans")
     ap.add_argument("dir")
+    ap.add_argument("--verbose", action="store_true")
     args = ap.parse_args()
 
     files = []
@@ -35,13 +61,22 @@ def main(argv=None):
     discontinuous_entity_counts = []
     for f in files:
         text_annotation = annotation.TextAnnotations(f)
-        total_entities += len(list(text_annotation.get_entities()))
-        discontinuous_entities = len(find_discontinuous(text_annotation))
-        discontinuous_entity_counts.append(discontinuous_entities)
+        entity_count = len(list(text_annotation.get_entities()))
+        discontinuous = find_discontinuous(text_annotation)
+        discontinuous_count = len(discontinuous)
+        total_entities += entity_count
+        discontinuous_entity_counts.append(discontinuous_count)
+        if args.verbose:
+            if total_entities == 0 or discontinuous_count == 0:
+                continue
+            print "\n" + "-" * 60
+            print "{} discontinous entities out of {} in {}".format(discontinuous_count, entity_count, f)
+            for e in discontinuous:
+                print_entity_mention(e, text_annotation.get_document_text())
 
-    total_discontinous_entities = sum(discontinuous_entity_counts)
-    print "Total number of entities: %d" % total_entities
-    print "Total number of discontinous entites: %d" % total_discontinous_entities
+    total_discontinuous_entities = sum(discontinuous_entity_counts)
+    print "Total number of entities: {}".format(total_entities)
+    print "Total number of discontinous entites: {}".format(total_discontinuous_entities)
 
 
 if __name__ == "__main__":
