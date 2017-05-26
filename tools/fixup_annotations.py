5858dc6b7055b4aa7a895b005d14ddb92569fd27
Derick Anderson <dericka@allenai.org>
2017-05-26 16:04:21 -0700

    trim_leading_determiners fixup step

--- tools/fixup_annotations.py
+++ tools/fixup_annotations.py
@@ -13,15 +13,35 @@ def remove_other_relations(doc):
     for relation in doc.relations:
         if relation.type == "other_relation":
             to_remove.append(relation)
-    logging.debug("{} `other_relation's to fixup in doc {}".format(len(to_remove), doc.document_id))
     for relation in to_remove:
         doc.remove_relation(relation)
+    logging.debug("{} `other_relation's to fixup in doc {}".format(len(to_remove), doc.document_id))   
+    return doc
+
+
+def trim_leading_determiners(doc):
+    count = 0
+    for entity in doc.entities:
+        start_token = doc[entity.spans[0][0]]
+        # `is_oov' is included because empirically spaCy will tag some valid
+        # entities as determiners if their surface forms are OOV
+        if start_token.tag_ == "DT" and not start_token.is_oov:
+            count += 1
+            first_span = (entity.spans[0][0] + 1, entity.spans[0][1])
+            if first_span[1] - first_span[0] == 0:
+                if len(entity.spans) == 1:
+                    logging.warn(u"Removing entity {} that is only a determiner".format(entity))
+                    doc.remove_entity(entity, force_remove_relations=True)
+                else:
+                    entity.set_spans(entity.spans[1:])
+            else:
+                entity.set_spans([first_span] + entity.spans[1:])
+    logging.debug("{} leading determiners to fixup in doc {}".format(count, doc.document_id))
     return doc
 
 
 def fixup_overlapping_annotations(doc):
     overlapping = ai2_common.find_overlapping(doc)
-    logging.debug("{} overlapping pairs to fixup in doc {}".format(len(overlapping), doc.document_id))
     for pair in overlapping:
         if pair[0].get_relations() and not pair[1].get_relations():
             remove = pair[1]
@@ -31,11 +51,13 @@ def fixup_overlapping_annotations(doc):
             remove = pair[0]
         else:
             remove = pair[1]
-        doc.remove_entity(remove)
+        doc.remove_entity(remove, force_remove_relations=True)
+    logging.debug("{} overlapping pairs to fixup in doc {}".format(len(overlapping), doc.document_id))
     return doc
 
 
 FIXUP_STEPS = [remove_other_relations,
+               trim_leading_determiners,
                fixup_overlapping_annotations]
 
 
