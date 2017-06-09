a7a1124c80a0953c5e45a736b72e7d69e979c9bd
Derick Anderson <dericka@allenai.org>
2017-06-09 08:00:24 -0700

    Kebab case

--- setup.py
+++ setup.py
@@ -1,11 +1,12 @@
 from setuptools import setup
 
-setup(name='ai2_brat',
+setup(name='ai2-brat',
       version='0.1',
       description='brat related utilities',
       url='http://github.com/allenai/brat',
       packages=['ai2_brat'],
       install_requires=["numpy",
                         "scipy",
-                        "spacy"],
+                        "spacy",
+                        "en-core-web-md"],
       zip_safe=False)
